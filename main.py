58c9f4a0dcdbc4d88ccbbe8b16c4485d3d591bb5
Ryota14-git <m-ryota0814@ezweb.ne.jp>
2025-01-12 12:47:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ from langchain_core.output_parsers import StrOutputParser
 # models
 from langchain_openai import ChatOpenAI
 
-###### dotenv を利用しない場合は消してください ######
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except ImportError:
-    import warnings
-    warnings.warn("dotenv not found. Please make sure to set your environment variables manually.", ImportWarning)
-################################################
-
-
 MODEL_PRICES = {
     "input": {
         "gpt-3.5-turbo": 0.5 / 1_000_000,
